c2ca9523fcc26ce945f452a6f86a9d5b50fd4ac0
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-29 16:23:40 +0530

    debugging..

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
     const currentScrollPos = window.pageYOffset;
     const navbar = document.getElementById("navbar");
+    // const navbar = document.getElementsByClassName("mnc");
     
     if (prevScrollPos < 100) {
         navbar.classList.remove("hidden");
