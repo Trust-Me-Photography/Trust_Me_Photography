67bcdf42093db612e42d59f17189c63f62bc6c96
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-29 12:46:58 +0530

    major fixes!

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
     const currentScrollPos = window.pageYOffset;
     const navbar = document.getElementById("navbar");
-
-    if (prevScrollPos > currentScrollPos) {
+    
+    if (prevScrollPos < 100) {
         navbar.classList.remove("hidden");
     } else {
         navbar.classList.add("hidden");
